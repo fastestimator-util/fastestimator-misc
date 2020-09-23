52cec3487f6c668dc12cc5525d895aa4c310212e
XMD <shawnmengdong@gmail.com>
2020-09-23 17:52:00 -0500

    Update entry_point.sh

--- sagemaker/FE1.1/RestoreWizard/entry_point.sh
+++ sagemaker/FE1.1/RestoreWizard/entry_point.sh
@@ -8,7 +8,7 @@ pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pyto
 pip install tensorflow==2.3.0
 
 # FE
-pip install git+https://github.com/fastestimator/fastestimator.git
+pip install fastestimator
 
 # training script, if you have specific training data, make use the $SM_CHANNEL_TRAINING
 fastestimator train mnist_tf.py --save_dir $SM_MODEL_DIR  --restore_dir /opt/ml/checkpoints/
