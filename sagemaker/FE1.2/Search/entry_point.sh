cd45521b7d3dbca636af28aa0e0eabd66deaa825
vbvg2008 <shawnmengdong@gmail.com>
2021-05-19 22:34:49 +0000

    update torch version

--- sagemaker/FE1.2/Search/entry_point.sh
+++ sagemaker/FE1.2/Search/entry_point.sh
@@ -4,8 +4,7 @@
 apt-get install -y --no-install-recommends libglib2.0-0 libsm6 libxrender1 libxext6
 
 # backends
-RUN pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install tensorflow==2.5.0
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
 # FE
 pip install fastestimator
--- sagemaker/FE1.2/Traceability/entry_point.sh
+++ sagemaker/FE1.2/Traceability/entry_point.sh
@@ -13,8 +13,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y graphviz texlive-latex-base texlive-latex-extra
 
 # backends
-RUN pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install tensorflow==2.5.0
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
 # FE
 pip install fastestimator
