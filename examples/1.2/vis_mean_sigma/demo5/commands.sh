0993864987a5ee2425afa8e2f78026f80b44ac6a
vbvg2008 <shawnmengdong@gmail.com>
2021-06-08 16:31:23 -0500

    added example

---
+++ examples/1.2/vis_mean_sigma/demo5/commands.sh
@@ -0,0 +1,12 @@
+
+fastestimator logs ./
+
+# for remote machine
+fastestimator logs ./ --save ./result.png
+
+
+fastestimator logs ./ --group_by _n 
+
+
+# for remote machine
+fastestimator logs ./ --group_by _n --save ./results2.png
